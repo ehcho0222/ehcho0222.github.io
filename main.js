d8ec02cdea7ef4a4795f77ea8deeb03574f5db82
ehcho0222 <64142236+ehcho0222@users.noreply.github.com>
2020-08-29 18:23:53 +0900

    ganada

--- main.js
+++
@@ -1,9 +0,0 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = '백멍이 귀엽다';
-var cuteness = 10000;
-var mod = 2;
-function multiply(num1, num2) {
-    var result = num1 * num2;
-    return result;
-}
-alert('백멍이의 귀여움: ' + multiply(cuteness, mod));
